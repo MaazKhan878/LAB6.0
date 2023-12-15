16bce71021939c7e5de2d8f3db97c2184d2ee460
Maaz_Khan <maazkhan0348810@gmail.com>
2023-12-16 03:59:51 +0500

    Funtion TOGRAYSCALE COMPLETED

--- colorUtils.c
+++ colorUtils.c
@@ -10,19 +10,28 @@ int max(int a, int b, int c) {
 int min(int a, int b, int c) {
     return (a < b) ? (a < c ? a : c) : (b < c ? b : c);
 }
+// This function return Error and the Value of diferent filter
+Error toGrayScale(int *r, int *g, int *b, Mode mode) {
+  // if the address or refrence pass to this function as 0 it will return the value 1
+  if(r == NULL || g == NULL || b == NULL ){
+    return ERROR_PRESENT;
+  }
+  // for AVERAGE TECHNIQUE
+  if (mode == AVERAGE) {
+    *r = *g = *b = (*r + *g + *b) / 3;
+  }
+  //FOR LIGHTNESS TECHNIQUE
+  else if (mode == LIGHTNESS) {
+    *r = *g = *b = (max(*r, *g, *b) + min(*r, *g, *b));
+  } 
+  //FOR LUMINOSITY TECHNIQUE
+  else if (mode == LUMINOSITY) {
+    *r = *g = *b = round(0.21 * *r + 0.72 * *g + 0.07 * *b);
+  }
 
-int forSepia(float value) {
-    return (value < 255) ? value : 255;
+  return NO_ERROR;
 }
+Error toSepia(int *r, int *g, int *b){
 
-int toGrayScale(int *r, int *g, int *b, Mode mode) {
-  int toGrayScale(int *r, int *g, int *b, Mode mode) {
-  //TODO: implement
 }
-
-int toSepia(int *r, int *g, int *b) {
-  //TODO: implement
-}
-}
-
-  
+ 
